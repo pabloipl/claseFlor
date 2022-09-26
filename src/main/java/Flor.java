978023a5dc247042447785489addea942290e00a
pabloipl <p.perez14@ufromail.cl>
2022-09-26 20:18:14 -0300

    commit

---
+++ src/main/java/Flor.java
@@ -0,0 +1,27 @@
+public class Flor {
+    String nombre;
+    String color;
+    int edad;
+    int petalos;
+
+    public void crecer(){
+
+    }
+
+    public void absorberLuz(){
+
+    }
+
+    public void absorberAgua(){
+
+    }
+
+    public void absorberDioxidoCarbono(){
+
+    }
+
+    public void liberarOxigeno(){
+
+    }
+}
+
